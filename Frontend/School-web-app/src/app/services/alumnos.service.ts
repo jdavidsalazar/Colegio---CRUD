21a8e6918cd978172be8b4e2ca6fa0bf348a7734
jdavidsalazar <juand.salazar.m@gmail.com>
2024-08-19 21:38:56 -0500

    add frontend angular proyect

---
+++ Frontend/School-web-app/src/app/services/alumnos.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AlumnosService {
+  private baseApiUrl = 'https://localhost:7068/api/v1/';
+
+  constructor(private http: HttpClient) {}
+
+  getAlumnos(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseApiUrl}alumnos`);
+  }
+
+  addAlumno(alumno: any): Observable<any> {
+    return this.http.post<any>(`${this.baseApiUrl}alumnos`, alumno);
+  }
+
+  updateAlumno(id: number, alumno: any): Observable<any> {
+    return this.http.put<any>(`${this.baseApiUrl}alumno/${id}`, alumno);
+  }
+
+  deleteAlumno(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.baseApiUrl}alumno/${id}`);
+  }
+}
